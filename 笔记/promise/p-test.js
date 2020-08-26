8aef08639caa8bd412c438afd99c41adfaed72b7
gechen <gechen@gstianfu.com>
2020-08-26 12:54:00 +0800

    test

--- 笔记/promise/p-test.js
+++ 笔记/promise/p-test.js
@@ -1,29 +1,104 @@
-class HD{
-  static PENDING = 'pending'
-  static FULFILLED = 'fufilled'
-  static REJECTED = 'rejected'
-  constructor(fn){
-    this.status = HD.PENDING
-    this.value = null
-    this.reason = null
+class HD {
+  // 定义状态值
+  static PENDING = "pending";
+  static FULFILLED = "fufilled";
+  static REJECTED = "rejected";
+  constructor(fn) {
+    this.status = HD.PENDING;
+    // 终值
+    this.value = null;
+    // 拒因
+    this.reason = null;
+    // 回调队列
+    this.callbacks = [];
     try {
-      fn(this.resolve.bind(this), this.reject.bind(this))
+      // 1. new Promise((resolve,reject)=>{}) 内部函数 fn是定义函数 , 此处的 resolve,reject为形参
+      // fn(this.resolve.bind(this), this.reject.bind(this)) 为函数调用，并传入了实参
+      fn(this.resolve.bind(this), this.reject.bind(this));
     } catch (error) {
-      this.reject(error)
+      this.reject(error);
     }
   }
-  resolve(value){
-    if(this.status === HD.PENDING){
-      this.status = HD.FULFILLED
-      this.value = value
+  resolve(value) {
+    // 先去看 then
+    // 因为 promise A+规范，resolve和 reject 状态改变后不可再次改变所以需要判断当前状态是否为 pending状态
+    if (this.status === HD.PENDING) {
+      // 改变状态
+      this.status = HD.FULFILLED;
+      // 将 resolve传入的终值赋值给 静态变量
+      this.value = value;
+      //
+      setTimeout(() => {
+        this.callbacks.map((cb) => {
+          cb.onFulfilled(this.value);
+        });
+      }, 0);
     }
-
   }
-  reject(reason){
-    if(this.status === HD.PENDING){
-      this.status = HD.REJECTED
-      this.reason = reason
+  reject(reason) {
+    if (this.status === HD.PENDING) {
+      this.status = HD.REJECTED;
+      this.reason = reason;
+      setTimeout(() => {
+        this.callbacks.map((cb) => {
+          cb.onRejected(this.reason);
+        });
+      }, 0);
     }
+  }
+  // then应为同步执行，但是 then的回调为异步 所以需要将 then的参数 函数执行放入异步执行，也就是 resolve和 reject内的 cb调用放入异步
+  then(onFulfilled, onRejected) {
+    // 因为 promise A+规范 then的两个参数必须为函数
+    if (typeof onFulfilled !== "function") {
+      onFulfilled = () => {};
+    }
+    if (typeof onRejected !== "function") {
+      onRejected = () => {};
+    }
+    return new HD((resolve, reject) => {
+      console.log('return HD');
+      // 因为 promise同步内大部分时候可能有异步任务，所以判断当前状态若为 pending的话
+      // 将onFulfilled或 onRejected的 定义函数放入回调队列，等待 resolve或 reject后再调用
+      if (this.status === HD.PENDING) {
+        // 将 then的回调异常抛出
+        this.callbacks.push({
+          onFulfilled: (value) => {
+            try {
+              onFulfilled(value);
+            } catch (error) {
+              onFulfilled(error);
+            }
+          },
+          onRejected: (reason) => {
+            try {
+              onRejected(reason);
+            } catch (error) {
+              onRejected(error);
+            }
+          },
+        });
+      }
+      if (this.status === HD.FULFILLED) {
+        // then中为异步所以放入 setTimeout内
+        setTimeout(() => {
+          try {
+            //第一个 then是在外部人为写了 resolve/ reject来改变状态，这里仍然需要改变状态
+            let result = onFulfilled(this.value);
+            resolve(result);
+          } catch (error) {
+            onRejected(error);
+          }
+        }, 0);
+      }
+      if (this.status === HD.REJECTED) {
+        setTimeout(() => {
+          try {
+            onRejected(this.reason);
+          } catch (error) {
+            onRejected(error);
+          }
+        }, 0);
+      }
+    });
   }
 }
-
--- 笔记/promise/promise-1.js
+++ 笔记/promise/promise-1.js
@@ -1,79 +1,92 @@
-const PENDING = "pending";
-const FULFILLED = "fulfilled";
-const REJECTED = "rejected";
+// const PENDING = "pending";
+// const FULFILLED = "fulfilled";
+// const REJECTED = "rejected";
 
-class AjPromise {
-  constructor(fn) {
-    // 当前状态
-    this.state = PENDING;
-    // 终值
-    this.value = null;
-    // 拒因
-    this.reason = null;
-    //成功状态回调队列
-    this.onFulfilledCallbacks = [];
-    //拒绝状态回调队列
-    this.onRejectCallbacks = [];
+// class AjPromise {
+//   constructor(fn) {
+//     // 当前状态
+//     this.state = PENDING;
+//     // 终值
+//     this.value = null;
+//     // 拒因
+//     this.reason = null;
+//     //成功状态回调队列
+//     this.onFulfilledCallbacks = [];
+//     //拒绝状态回调队列
+//     this.onRejectCallbacks = [];
 
-    //成功状态回调
-    const resolve = (value) => {
-      // 使用macro-task机制(setTimeout),确保onFulfilled异步执行,且在 then 方法被调用的那一轮事件循环之后的新执行栈中执行。
-      setTimeout(() => {
-        // 只有在 PENDING状态才能变为 FULFILLED状态
-        if (this.state === PENDING) {
-          this.state = FULFILLED;
-          // 终值赋值
-          this.value = value;
-          console.log('resolve');
-          this.onFulfilledCallbacks.map((cb) => {
-            console.log("cb");
-            this.value = cb(this.value);
-          });
-        }
-      });
-    };
-    const reject = (reason) => {
-      // 使用macro-task机制(setTimeout),确保onRejected异步执行,且在 then 方法被调用的那一轮事件循环之后的新执行栈中执行。 (满足要求 -> 调用时机)
-      setTimeout(() => {
-        if (this.state === PENDING) {
-          // pending(等待态)迁移至 fulfilled(拒绝态),保证调用次数不超过一次。
-          this.state = REJECTED;
-          //拒因
-          this.reason = reason;
-          this.onRejectedCallbacks.map((cb) => {
-            this.reason = cb(this.reason);
-          });
-        }
-      });
-    };
+//     //成功状态回调
+//     const resolve = (value) => {
+//       // 使用macro-task机制(setTimeout),确保onFulfilled异步执行,且在 then 方法被调用的那一轮事件循环之后的新执行栈中执行。
+//       setTimeout(() => {
+//         // 只有在 PENDING状态才能变为 FULFILLED状态
+//         if (this.state === PENDING) {
+//           this.state = FULFILLED;
+//           // 终值赋值
+//           this.value = value;
+//           console.log('resolve');
+//           this.onFulfilledCallbacks.map((cb) => {
+//             console.log("cb");
+//             this.value = cb(this.value);
+//           });
+//         }
+//       });
+//     };
+//     const reject = (reason) => {
+//       // 使用macro-task机制(setTimeout),确保onRejected异步执行,且在 then 方法被调用的那一轮事件循环之后的新执行栈中执行。 (满足要求 -> 调用时机)
+//       setTimeout(() => {
+//         if (this.state === PENDING) {
+//           // pending(等待态)迁移至 fulfilled(拒绝态),保证调用次数不超过一次。
+//           this.state = REJECTED;
+//           //拒因
+//           this.reason = reason;
+//           this.onRejectedCallbacks.map((cb) => {
+//             this.reason = cb(this.reason);
+//           });
+//         }
+//       });
+//     };
 
-    try {
-      fn(resolve, reject);
-    } catch (err) {
-      reject(err);
-    }
-  }
-  then(onFulfilled, onRejected) {
-    console.log("then");
-    if (typeof onFulfilled === "function") {
-      this.onFulfilledCallbacks.push(onFulfilled);
-    }
-    if (typeof onRejected === "function") {
-      this.onRejectedCallbacks.push(onRejected);
-    }
-    // 返回this支持then 方法可以被同一个 promise 调用多次
-    return this;
-  }
-}
+//     try {
+//       fn(resolve, reject);
+//     } catch (err) {
+//       reject(err);
+//     }
+//   }
+//   then(onFulfilled, onRejected) {
+//     console.log("then");
+//     if (typeof onFulfilled === "function") {
+//       this.onFulfilledCallbacks.push(onFulfilled);
+//     }
+//     if (typeof onRejected === "function") {
+//       this.onRejectedCallbacks.push(onRejected);
+//     }
+//     // 返回this支持then 方法可以被同一个 promise 调用多次
+//     return this;
+//   }
+// }
 
-new AjPromise((resolve, reject) => {
-  console.log('AjPromise');
-  resolve(1);
-})
-  .then((res) => {
-    // console.log(res, "then-1");
-    return res + 1;
-  })
-  .then((res) => {
-    // console.log(res, "then-2");
-  });
+// new AjPromise((resolve, reject) => {
+//   console.log('AjPromise');
+//   resolve(1);
+// })
+//   .then((res) => {
+//     // console.log(res, "then-1");
+//     return res + 1;
+//   })
+//   .then((res) => {
+//     // console.log(res, "then-2");
+//   });
+
+
+/* 
+  1. resolve里的 setTimeout   push进了回调队列中的函数调用到 “宏任务队列”
+  2. then中的 new HD又一次调用了 HD类，并且这次的 HD调用又调用了 HD类中的 executor函数
+    但是因为在外部的 new HD中 this.status已经改变为 fulfilled 所以此次进入了 this.status == HD.FULFILLED判断
+    将 this.parse 推入 “宏任务队列”
+  3. 内部 new HD返回的 promise没有改变状态 所以是 pending
+  4. 此时进入了第二个 then，又一次调用内部new HD 因为第二个 promise内部 new HD返回的 promise没有改变状态 所以进入 pending判断
+  5. 第二个 then将 onFulfilled/onRejected，推入 callbacks等待执行，所返回的 promise仍然是 pending
+
+  6. 此时所有同步人物制定完毕，开始执行 1. 中的回调任务
+*/
